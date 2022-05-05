730b1877eae02b9753840109f8a7e1028c772337
Praan26 <arpanap102@gmail.com>
2022-05-05 17:41:52 +0530

    [Arpan] Add: Binary search using a text file added

---
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using AlgorithmPrograms;
+using AlgorithmPrograms.Utility;
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("welcome to the program");
+        bool check=true;
+        const string path = @"D:\PracticeProblem\AlgorithmPrograms\AlgorithmPrograms\AlgorithmPrograms\Utility\BinarySearchFile.txt";
+        BinarySearch binary = new BinarySearch();
+        Console.WriteLine("1.Binary Search");
+        while(check)
+        {
+            Console.WriteLine("Take an option to execute");
+            int opt=Convert.ToInt32(Console.ReadLine());
+            switch(opt)
+            {
+                case 1:
+                    string[] input_Array=File.ReadAllLines(path);
+                    Console.WriteLine("Enter a word to search");
+                    string word=Console.ReadLine();
+                    binary.Search(input_Array,word);
+                    break;
+            }
+        }
+    }
+}
---
+++ AlgorithmPrograms/AlgorithmPrograms/Utility/BinarySearch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms.Utility
+{
+    public class BinarySearch
+    {
+        public void Search(string[] input_Array, string value)
+        {
+            int min=0;
+            int max = input_Array.Length - 1;
+            while(min <= max)
+            {
+                int mid = (min+max)/2;
+                if(input_Array[mid] == value)
+                {
+                    Console.WriteLine("The word is at {0} position",mid);
+                }
+                if (value.CompareTo(input_Array[mid]) >0)
+                {
+                    max = mid-1;
+                    Console.WriteLine("The word is at {0} position",max);
+                }
+                else
+                {
+                    min = mid+1;
+                    Console.WriteLine("The word is at {0} position", min);
+                }
+            }
+        }
+    }
+}
